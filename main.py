1ef9a923bfd71e0b8a59a6d54ffa4141f3de3670
ManoelEric1212 <manoeleric59@gmail.com>
2023-07-24 11:26:52 -0300

    refactor(Documentacao): :memo: Atualizando descricao da documentacao swagger

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import uvicorn
 from contas_a_pagar_e_receber.routes import contas_a_pagar_e_receber
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Contas API",
+    version="1.0.0",
+    description="API desenvolvida para aprender melhor o framework FastAPI",
+)
 app.include_router(contas_a_pagar_e_receber.router)
 
