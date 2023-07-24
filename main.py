3213d0627dce6387167b1e6ac928bd8de1223fd9
ManoelEric1212 <manoeleric59@gmail.com>
2023-07-24 09:56:02 -0300

    feat(Routes): :zap: Segmentando estrutura de routes no projeto

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from contas_a_pagar_e_receber.routes import contas_a_pagar_e_receber
 
 
 app = FastAPI()
 
-@app.get("/")
-def teste_de_requisicao() -> str:
-    return "Servidor rodando !"
+app.include_router(contas_a_pagar_e_receber.router)
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=3333)
