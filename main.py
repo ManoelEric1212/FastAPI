41f097673115f63e838f71bfb8b955c6762f966a
ManoelEric1212 <manoeleric59@gmail.com>
2023-07-24 11:02:52 -0300

    perf(Server): :zap: Configurando uvicorn com reload

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ from contas_a_pagar_e_receber.routes import contas_a_pagar_e_receber
 
 
 app = FastAPI()
-
 app.include_router(contas_a_pagar_e_receber.router)
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=3333)
+
