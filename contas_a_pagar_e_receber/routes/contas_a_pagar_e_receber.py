6f647a96640b4d3928b035af86e4e348d1bac8d8
ManoelEric1212 <manoeleric59@gmail.com>
2023-07-24 11:08:36 -0300

    feat(Configurando modelos com pydantic): :art: Adicionando modelos de entrada-saida com pydantic

--- contas_a_pagar_e_receber/routes/contas_a_pagar_e_receber.py
+++ contas_a_pagar_e_receber/routes/contas_a_pagar_e_receber.py
@@ -1,14 +1,43 @@
+from decimal import Decimal
+from typing import List
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix="/contas-a-pagar-e-receber")
-@router.get("/")
+
+class ContasPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str
+class ContasPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str
+
+@router.get("/", response_model=List[ContasPagarReceberResponse])
 def listar_contas():
     return [
-        {
-            "contas1": "contas1",
-            "contas2": "contas2",
-            "contas3": "contas3",
-            "contas4": "contas4",
-         }
+        ContasPagarReceberResponse(
+        id=1,
+        descricao="aluguel",
+        valor=1000.50,
+        tipo="PAGAR"
+        ),
+        ContasPagarReceberResponse(
+        id=1,
+        descricao="Carro",
+        valor=5000.50,
+        tipo="PAGAR"
+        ),
     ]
+@router.post("/", response_model=ContasPagarReceberResponse, status_code=201)
+def criar_conta(conta: ContasPagarReceberRequest):
+    return ContasPagarReceberResponse(
+        id=3,
+        descricao= conta.descricao,
+        valor=conta.valor,
+        tipo=conta.tipo
+    )
+    
